faf1a26b2dd2e547cec11d6976df42effd61c7d3
abdul-rahman <abdul.rehman@invozone.com>
2021-04-13 14:13:32 +0500

    added folder structure for components

---
+++ src/layouts/Admin.js
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+
+const Admin = () => {
+    return ( 
+        <div>
+            <Navbar />
+            <Sidebar />
+        </div>
+     );
+}
+ 
+export default Admin;
